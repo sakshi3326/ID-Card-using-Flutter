45177bcb50868cdcbe8a391693d1b189b20eb187
sakshi3326 <saqkshiiitian123@gmail.com>
2022-08-11 00:43:19 +0530

    background set

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,27 @@ class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text('ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
+       body: Padding(
+         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+         child: Column(
+           children: <Widget>[
+             Text(
+               'NAME',
+               style: TextStyle(
+                 color: Colors.grey,
+
+               ),
+             )
+           ],
+         ),
+       ),
     );
   }
 }
